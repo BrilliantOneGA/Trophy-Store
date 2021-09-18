a7f225535edb6c636a96141b7f7f46ba0183d240
moore <moore_lawrence@columbusstate.edu>
2021-09-18 18:59:07 -0400

    Practice with TrophyStore

---
+++ UI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Models;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to Trophy World!");
+        }
+    }
+}
